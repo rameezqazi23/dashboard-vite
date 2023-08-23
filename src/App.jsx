996ed8eb03d2dabc4d0091b1a3b6b4e894f6696c
rameezqazi23 <rameez.qazi23@gmail.com>
2023-08-24 02:15:31 +0500

    creting layout using tremor as charts and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1 className='text-4xl text-[#1a1a1a] font-bold text-center'>Hello From Vite</h1>
+    <main className="flex">
+      <Sidebar />
+      <div className="flex flex-col flex-1 relative">
+        <Navbar />
       </div>
-    </>
+
+      
+    </main>
   )
 }
 
